17de40fc2e6b89fba56e8c20772aba62cf39bd36
tomas-20 <tacuna20@stuy.edu>
2020-12-09 13:13:36 -0500

    filled in the stuff for NoNullArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
-  
+  public NoNullArrayList() {
+    super();
+  }
+  public NoNullArrayList(int n) {
+    super(n);
+  }
+  public boolean add(T item) {
+    if (item == null) {
+      throw new IllegalArgumentException("you can't have a null in a NoNullArrayList");
+    }
+    return super.add(item);
+  }
 }
