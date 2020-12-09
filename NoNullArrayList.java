8e64e0af427b0094c1487bc2d055022e0a281f28
tomas-20 <tacuna20@stuy.edu>
2020-12-09 13:25:47 -0500

    made the second add function for NoNullArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,10 +6,17 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(int n) {
     super(n);
   }
-  public boolean add(T item) {
+  private void check(T item) {
     if (item == null) {
       throw new IllegalArgumentException("you can't have a null in a NoNullArrayList");
     }
+  }
+  public boolean add(T item) {
+    check(item);
     return super.add(item);
+  }
+  public void add(int n, T item) {
+    check(item);
+    super.add(n, item);
   }
 }
